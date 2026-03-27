5a2318c0411a94b809fca836c3bee5c9aec2d703
pploss <pplo9184@sina.com>
2026-03-27 10:56:10 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -76,7 +76,7 @@ lazy_static::lazy_static! {
         map.insert("custom-rendezvous-server".to_string(), server_addr);
         //中继服务器
         //map.insert("relay-server".to_string(), "baidu.com.cn:21117".to_string());
-        let reserver_addr = format!("{}:{}", RENDEZVOUS_SERVERS[0], RELAY_PORT);
+        let reserver_addr = format!("{}:{}", RENDEZVOUS_SERVERS[0], relay_port());
         map.insert("relay-server".to_string(), reserver_addr);
         //API服务器
         //map.insert("api-server".to_string(), "http://baidu.com.cn:21114".to_string());
@@ -155,29 +155,29 @@ pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
     _ => PUBLIC_RS_PUB_KEY,
 };
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const PUBLIC_RENDEZVOUS_PORT: i32 = 11116;
+pub const RENDEZVOUS_PORT: i32 = 11116;
+pub const PUBLIC_RENDEZVOUS_PORT: i32 = 21116;
 pub fn rendezvous_port() -> i32 {
     option_env!("RENDEZVOUS_PORT")
         .and_then(|v| v.parse::<i32>().ok())
         .unwrap_or(PUBLIC_RENDEZVOUS_PORT)
 }
-pub const RELAY_PORT: i32 = 21117;
-pub const PUBLIC_RELAY_PORT:i32 = 11117;
+pub const RELAY_PORT: i32 = 11117;
+pub const PUBLIC_RELAY_PORT:i32 = 21117;
 pub fn relay_port() -> i32 {
     option_env!("RELAY_PORT")
         .and_then(|v| v.parse::<i32>().ok())
         .unwrap_or(PUBLIC_RELAY_PORT)
 }
-pub const WS_RENDEZVOUS_PORT: i32 = 21118;
-pub const PUBLIC_WS_RENDEZVOUS_PORT: i32 = 11118;
+pub const WS_RENDEZVOUS_PORT: i32 = 11118;
+pub const PUBLIC_WS_RENDEZVOUS_PORT: i32 = 21118;
 pub fn wsrendezvous_port() -> i32 {
     option_env!("WS_RENDEZVOUS_PORT")
         .and_then(|v| v.parse::<i32>().ok())
         .unwrap_or(PUBLIC_WS_RENDEZVOUS_PORT)
 }
-pub const WS_RELAY_PORT: i32 = 21119;
-pub const PUBLIC_WS_RELAY_PORT:i32 = 11119;
+pub const WS_RELAY_PORT: i32 = 11119;
+pub const PUBLIC_WS_RELAY_PORT:i32 = 21119;
 pub fn wsrelay_port() -> i32 {
     option_env!("WS_RELAY_PORT")
         .and_then(|v| v.parse::<i32>().ok())
